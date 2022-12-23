65fbca9b75544a85b9e0e4601ea90fc0d21f1cb4
Deadloct <rad@deadloct.com>
2022-12-22 17:25:17 -0800

    Fetching pages of assets for print method

--- client.go
+++ client.go
@@ -8,6 +8,8 @@ import (
 	"github.com/immutable/imx-core-sdk-golang/imx/api"
 )
 
+const MaxAssetsPerReq = 200
+
 type Client struct {
 	imxClient *imx.Client
 }
@@ -32,17 +34,57 @@ func (c *Client) Stop() {
 }
 
 func (c *Client) PrintAssetCounts(ctx context.Context, addr string) {
-	req := c.imxClient.NewListAssetsRequest(ctx).Collection(addr).PageSize(9999)
-
-	resp, err := c.imxClient.ListAssets(&req)
+	assets, err := c.GetAssets(ctx, addr, nil, "")
 	if err != nil {
-		log.Panicf("error when listing assets: %v\n", err)
+		log.Printf("failed to get assets: %v\n", err)
+		return
 	}
 
 	rarityCounts := make(map[string]int, 4)
-	for _, asset := range resp.Result {
-		rarityCounts[asset.Metadata["Rarity"].(string)]++
+	for _, asset := range assets {
+		rarity, ok := asset.Metadata["Rarity"].(string)
+		if !ok {
+			log.Printf("failed to find rarity for asset %#v", asset)
+			continue
+		}
+
+		if asset.Name.IsSet() {
+			rarityCounts[rarity]++
+		} else {
+			log.Println("asset skipped since it has no name and must be messed up")
+		}
 	}
 
 	log.Printf("Rarities for collection %s: %#v\n", addr, rarityCounts)
+}
+
+func (c *Client) GetAssets(ctx context.Context, addr string, assets []api.AssetWithOrders, cursor string) ([]api.AssetWithOrders, error) {
+	log.Printf("fetching %d more assets\n", MaxAssetsPerReq)
+
+	req := c.imxClient.NewListAssetsRequest(ctx).
+		Collection(addr).
+		Cursor(cursor).
+		PageSize(MaxAssetsPerReq).
+		OrderBy("updated_at")
+
+	resp, err := c.imxClient.ListAssets(&req)
+
+	if err != nil {
+		log.Printf("failed to get assets for addr %s: %v", addr, err)
+		return nil, err
+	}
+
+	first := *resp.Result[0].UpdatedAt.Get()
+	last := *resp.Result[len(resp.Result)-1].UpdatedAt.Get()
+	log.Printf("fetched %v assets from %v to %v\n", len(resp.Result), first, last)
+
+	assets = append(assets, resp.Result...)
+	cursor = resp.Cursor
+
+	if resp.Remaining > 0 {
+		log.Printf("fetching more asset pages (%v)...", resp.Remaining)
+		return c.GetAssets(ctx, addr, assets, cursor)
+	}
+
+	return assets, nil
 }
